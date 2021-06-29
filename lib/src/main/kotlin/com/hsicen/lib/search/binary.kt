69a1d0f159f3fb1f470a255438ffffcf538577f0
hsicen <codinghuang@163.com>
2021-06-29 18:14:05 +0800

    Update binary.kt
    二分查找的应用

--- lib/src/main/kotlin/com/hsicen/lib/search/binary.kt
+++ lib/src/main/kotlin/com/hsicen/lib/search/binary.kt
@@ -1,5 +1,7 @@
 package com.hsicen.lib.search
 
+import kotlin.math.abs
+
 /**
  * 作者：hsicen  6/16/21 09:26
  * 邮箱：codinghuang@163.com
@@ -16,6 +18,12 @@ package com.hsicen.lib.search
  *
  * 时间复杂度：O(logn)
  * 空间复杂度：O(1)
+ *
+ * 二分查找形变问题：
+ * 1.查找第一个等于给定值的元素
+ * 2.查找最后一个等于给定值的元素
+ * 3.查找第一个大于等于给定值的元素
+ * 4.查找最后一个小于等于给定值的元素
  */
 
 fun main() {
@@ -24,10 +32,21 @@ fun main() {
   println("10的下标：${bSearch2(items, 10)}")
   println("15的下标：${bSearch1(items, 15)}")
   println("15的下标：${bSearch2(items, 15)}")
+
+  println("1的平方根：${mySqrt2(1f, 1)}")
+  println("2的平方根：${mySqrt2(2f, 2)}")
+  println("3的平方根：${mySqrt2(3f, 3)}")
+  println("8的平方根：${mySqrt2(8f, 4)}")
+  println("9的平方根：${mySqrt2(9f, 5)}")
+
+  val items2 = intArrayOf(1, 2, 3, 4, 5, 6, 8, 8, 8, 8, 8, 11, 18)
+  println(findFirstEquals(items2, 3))
+  println(findFirstEquals(items2, 8))
+  println(findFirstEquals(items2, 22))
 }
 
 /**
- * 二分查找非递归实现
+ * 二分查找非递归实现(数组有序，无重复元素)
  * @param items 数据
  * @param item 被查找的元素
  * @return 查找到的位置下标
@@ -44,7 +63,6 @@ private fun bSearch1(items: IntArray, item: Int): Int {
     //val mid = low + (high - low) shr 1 位运算右移
     val mid = low + ((high - low) shr 1)
     when {
-      //使用high/low=mid有可能发生死循环
       items[mid] == item -> return mid
       items[mid] > item -> high = mid - 1
       items[mid] < item -> low = mid + 1
@@ -55,7 +73,7 @@ private fun bSearch1(items: IntArray, item: Int): Int {
 }
 
 /**
- * 二分查找递归实现
+ * 二分查找递归实现(数组有序，无重复元素)
  * @param items 数据
  * @param item 被查找的元素
  * @return 查找到的位置下标
@@ -75,3 +93,88 @@ private fun bSearch2(items: IntArray, item: Int): Int {
   return search(items, item, 0, items.size - 1)
 }
 
+
+/**
+ * 求一个数的平方根(保留整数)
+ */
+private fun mySqrt(item: Int): Int {
+  if (item <= 0) return -1
+  if (1 == item) return item
+
+  var low = 0
+  var high = item
+  var result = -1
+  while (low <= high) {
+    val mid = low + ((high - low) / 2)
+
+    if (mid <= item / mid) {
+      result = mid
+      low = mid + 1
+    } else {
+      high = mid - 1
+    }
+  }
+
+  return result
+}
+
+/**
+ * 求一个数的平方根(保留整数)
+ * 牛顿迭代法
+ */
+private fun mySqrt2(x: Float, count: Int): Float {
+  if (x <= 0) return 0f
+  if (x == 1f) return 1f
+
+  //精度
+  val e = 1 shr (if (count < 0) 0 else count)
+  var ret = x
+  var t = (ret + x / ret) / 2
+  while (abs(ret - t) > e) {
+    ret = t
+    t = (ret + x / ret) / 2
+  }
+
+  return ret
+}
+
+/**
+ * 查找第一个等于给定值的元素
+ * @param items 数据
+ * @param item 被查找的元素
+ * @return 返回数组下标
+ */
+private fun findFirstEquals(items: IntArray, item: Int): Int {
+  if (items.isEmpty()) return -1
+  var low = 0
+  var high = items.size - 1
+
+  while (low <= high) {
+    val mid = low + ((high - low) shr 1)
+    when {
+      items[mid] > item -> high = mid - 1
+      items[mid] < item -> low = mid + 1
+      else -> {
+        if (0 == mid || items[mid - 1] != item) {
+          return mid
+        } else {
+          high = mid - 1
+        }
+      }
+    }
+  }
+
+  return -1
+}
+
+/**
+ * 查找最后一个等于给定值的元素
+ * @param items 数据
+ * @param item 被查找的元素
+ * @return 返回数组下标
+ */
+private fun findLastEquals(items: IntArray, item: Int): Int {
+
+
+  return -1
+}
