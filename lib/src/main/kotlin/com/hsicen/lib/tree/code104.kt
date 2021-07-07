02647c7ea336dedbdeacc5593f5bf3af73d75c0e
hsicen <codinghuang@163.com>
2021-07-07 10:58:31 +0800

    树的相关操作

---
+++ lib/src/main/kotlin/com/hsicen/lib/tree/code104.kt
@@ -0,0 +1,19 @@
+package com.hsicen.lib.tree
+
+import kotlin.math.max
+
+/**
+ * 作者：hsicen  7/6/21 18:16
+ * 邮箱：codinghuang@163.com
+ * 作用：
+ * 描述：求二叉树的最大深度
+ */
+
+/**
+ * 求二叉树的高度(DFS)
+ */
+private fun maxDepth(root: BinaryTree?): Int {
+    if (null == root) return 0
+
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
+}
---
+++ lib/src/main/kotlin/com/hsicen/lib/tree/code450.kt
@@ -0,0 +1,53 @@
+package com.hsicen.lib.tree
+
+/**
+ * 作者：hsicen  7/7/21 09:25
+ * 邮箱：codinghuang@163.com
+ * 作用：
+ * 描述：删除二叉搜索树中的节点
+ */
+
+private fun deleteNode(root: BinaryTree?, data: Int): BinaryTree? {
+    if (null == root) return root
+    var head = root
+    var parent: BinaryTree? = null
+
+    //查找待删除节点
+    while (null != head && head.data != data) {
+        parent = head
+        head = if (head.data > data) {
+            head.left
+        } else head.right
+    }
+
+    //删除节点逻辑
+    if (null == head) return root
+    if (head.left != null && head.right != null) {
+        //删除的有左右子节点 找到左子树的最大节点或右子树的最小节点
+
+
+    } else {
+        //删除的只有左节点或右节点或叶子节点
+        val child = when {
+            head.left != null -> head.left
+            head.right != null -> head.right
+            else -> null
+        }
+
+        when {
+            parent?.left?.data == data -> parent.left = child
+            parent?.right?.data == data -> parent.right = child
+            else -> parent = child
+        }
+    }
+
+    return root
+}
+
+fun main() {
+    val node1 = BinaryTree(3, BinaryTree(2), BinaryTree(4))
+    val node2 = BinaryTree(6, null, BinaryTree(7))
+    val root = BinaryTree(5, node1, node2)
+
+    deleteNode(root, 8)
+}
