aab3a45c12e685c52fadd20468d697dda6c4777f
hsicen <codinghuang@163.com>
2021-07-12 14:31:33 +0800

    AC匹配算法实现关键词过滤

--- lib/src/main/kotlin/com/hsicen/lib/tree/Trie.kt
+++ lib/src/main/kotlin/com/hsicen/lib/tree/Trie.kt
@@ -1,5 +1,7 @@
 package com.hsicen.lib.tree
 
+import java.util.*
+
 /**
  * 作者：hsicen  7/12/21 10:35
  * 邮箱：codinghuang@163.com
@@ -16,7 +18,7 @@ package com.hsicen.lib.tree
 class TrieNode(
     val data: Char,
     val child: Array<TrieNode?> = Array(26) { null },
-    var isEndingChar: Boolean = false,
+    var isEndingChar: Boolean = false
 )
 
 class Trie {
@@ -47,5 +49,104 @@ class Trie {
         }
 
         return p?.isEndingChar != false
+    }
+}
+
+/**
+ * AC 自动机(多模式串匹配算法)和 Trie 树实现敏感词过滤
+ *
+ */
+class AcNode(
+    val data: Char,
+    val child: Array<AcNode?> = Array(26) { null },
+    var isEndingChar: Boolean = false, // 是否为结尾字符串
+    var length: Int = -1, //当 isEndingChar=true 时记录模式串长度
+    var fail: AcNode? = null // 失败指针
+)
+
+class AcTrie {
+    private val root = AcNode('/')
+
+    fun insert(text: String) {
+        var p: AcNode? = root
+        for (i in text) {
+            val index = i - 'a'
+            if (p?.child?.get(index) == null) {
+                val newNode = AcNode(i)
+                p?.child?.set(index, newNode)
+            }
+            p = p?.child?.get(index)
+        }
+        p?.isEndingChar = true
+    }
+
+    fun search(pattern: String): Boolean {
+        var p: AcNode? = root
+        for (i in pattern) {
+            val index = i - 'a'
+            if (p?.child?.get(index) == null) {
+                return false
+            }
+
+            p = p.child[index]
+        }
+
+        return p?.isEndingChar != false
+    }
+
+    fun buildFailurePointer() {
+        val queue = LinkedList<AcNode>()
+        root.fail = null
+        queue.add(root)
+
+        while (queue.isNotEmpty()) {
+            val p = queue.remove()
+            repeat(26) { i ->
+                p.child[i]?.let { pc ->
+                    if (p == root) {
+                        pc.fail = root
+                    } else {
+                        var q = p.fail
+                        while (q != null) {
+                            val qc = q.child[pc.data - 'a']
+                            if (qc != null) {
+                                pc.fail = qc
+                                break
+                            }
+                            q = q.fail
+                        }
+
+                        if (q == null) {
+                            pc.fail = root
+                        }
+                    }
+                    queue.add(pc)
+                }
+            }
+        }
+    }
+
+    /*** 在主串[source]中进行匹配*/
+    fun match(source: String) {
+        val n = source.length
+        var p: AcNode? = root
+
+        repeat(n) { i ->
+            val idx = source[i] - 'a'
+            while (p?.child?.get(idx) == null && p != root) {
+                p = p?.fail
+            }
+
+            p = p?.child?.get(idx)
+            if (p == null) p = root
+            var tmp = p
+            while (tmp != root) {
+                if (tmp?.isEndingChar == true) {
+                    val pos = i - tmp.length + 1
+                    println("匹配起始下标: $pos , 长度: ${tmp.length}")
+                }
+                tmp = tmp?.fail
+            }
+        }
     }
 }
