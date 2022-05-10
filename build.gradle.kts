4e82b203ad1ba9c7e2c0bbd9e7ea80991a20f45a
hsicen <huangsc4@lenovo.com>
2022-05-10 10:41:39 +0800

    update gradle configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-buildscript {
-  repositories {
-    google()
-    mavenCentral()
-  }
-
-  dependencies {
-    classpath("com.android.tools.build:gradle:7.1.2")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
-  }
-}
-
 allprojects {
   repositories {
     google()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,22 @@ include(":design")
 include(":letcode")
 include(":javaguide")
 include(":thinkjava")
+
+pluginManagement {
+  repositories {
+    gradlePluginPortal()
+    google()
+    mavenCentral()
+  }
+
+  resolutionStrategy {
+    eachPlugin {
+      when (requested.id.namespace) {
+        "com.android" -> useModule("com.android.tools.build:gradle:7.1.2")
+        "org.jetbrains.kotlin" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20")
+        "org.gradle" -> {} // println("官方插件:${requested.id}")
+        else -> useModule("${requested.id}:${requested.id}.gradle.plugin:${requested.version}")
+      }
+    }
+  }
+}
