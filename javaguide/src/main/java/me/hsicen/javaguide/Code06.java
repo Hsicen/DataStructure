a6b18a5447b4ca5e73e302c0c6fdd5649b075fa0
hsicen <codinghuang@163.com>
2022-05-19 18:33:36 +0800

    第6节：浮点数

---
+++ javaguide/src/main/java/me/hsicen/javaguide/Code06.java
@@ -0,0 +1,14 @@
+package me.hsicen.javaguide;
+
+/******====== 浮点数 ======******/
+public class Code06 {
+
+  public static void main(String[] args) {
+    System.out.println(100000000.0f);
+    float f2 = 1.3E23f;
+    System.out.println(f2);
+
+    float positiveInfinity = Float.POSITIVE_INFINITY;
+
+  }
+}
